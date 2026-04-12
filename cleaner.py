b50c47bbf089758fd40f4834d6a951649e524eb5
Emmanuel Ubah <emmaubah2006@gmail.com>
2026-04-12 18:49:01 -0400

    added two dataframes

--- cleaner.py
+++ cleaner.py
@@ -4,12 +4,14 @@ def clean_data(data):
     within this module. Note that this code will be *slightly* different
     from your original function.
     """
-    cleaned = []
+    # clean_data = [float(item) for item in data if item != "minutes"]
+    clean_data = []
 
     for item in data:
         if item != "minutes":
-            cleaned.append(float(item))
+            clean_data.append(float(item.strip()))
     
-    return cleaned
+
+    return clean_data
 
 # print(clean_data(['minutes\n', '11.6\n', '14.06\n', '11.7\n', '11.41\n', '9.44\n', '11.74\n']))
